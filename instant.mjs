971eba7a4788b72abaf43601a878c2ebd383802c
Shohei Yokoyama <1260486+abarth500@users.noreply.github.com>
2022-04-01 11:34:28 +0900

    code format

--- instant.mjs
+++ instant.mjs
@@ -14,30 +14,14 @@ const { terminal } = terminal_kit;
 program
     .requiredOption('-i, --id <id>', 'Query Identifier')
     .requiredOption('-u, --url <url>', 'URL of WebSocket Server')
-    .requiredOption(
-        '-t, --term <yyyy/mm/ddThh:mm-yyyy/mm/ddThh:mm>',
-        'Search Term'
-    )
-    .addOption(
-        new Option('--keywords-match <method>', 'Text Match Method')
-            .choices(['text-and', 'text-or', 'RegExp'])
-            .default('text-or', 'Text OR')
-    )
-    .requiredOption(
-        '-k, --keywords <word...>',
-        'Comma Separated Search Keywords'
-    )
-    .addOption(
-        new Option('-l, --lang <lang>', 'Language').choices(['ja', 'en'])
-    )
+    .requiredOption('-t, --term <yyyy/mm/ddThh:mm-yyyy/mm/ddThh:mm>', 'Search Term')
+    .addOption(new Option('--keywords-match <method>', 'Text Match Method').choices(['text-and', 'text-or', 'RegExp']).default('text-or', 'Text OR'))
+    .requiredOption('-k, --keywords <word...>', 'Comma Separated Search Keywords')
+    .addOption(new Option('-l, --lang <lang>', 'Language').choices(['ja', 'en']))
     .option('-m, --mask <mask>', 'JSON Mask (https://www.npmjs.com/package/json-mask)')
     .option('--ignore-retweet', 'Ignore Retweet')
     .option('--only-retweet', 'Only Retweet')
-    .addOption(
-        new Option('-g, --giveaway <method>', 'Upload Method')
-            .choices(['no', 'local', 'webdav'])
-            .default('local', 'Copy to anywhere in the local')
-    )
+    .addOption(new Option('-g, --giveaway <method>', 'Upload Method').choices(['no', 'local', 'webdav']).default('local', 'Copy to anywhere in the local'))
     .option('--jst', 'Convert create_at to JST')
     .option('-d, --destination <url-or-path>', 'Save Location')
     .option('-n, --user <username>', 'Username for Webdav Server')
@@ -49,9 +33,7 @@ const options = program.opts();
 let password;
 if (options.giveaway == 'webdav') {
     if (!options.destination) {
-        terminal(
-            'ERROR: ' + 'WebDAV URL NOT FOUND. Please specify --destination.\n'
-        );
+        terminal('ERROR: ' + 'WebDAV URL NOT FOUND. Please specify --destination.\n');
         terminal.processExit();
     }
     terminal('Password for the webdav server:');
@@ -64,9 +46,7 @@ if (options.giveaway == 'webdav') {
         if (!options.destination) {
             options.destination = path.normalize('~/exhaustive-twitter');
         } else {
-            options.destination = path.isAbsolute(options.destination)
-                ? options.destination
-                : path.resolve(process.cwd(), options.destination);
+            options.destination = path.isAbsolute(options.destination) ? options.destination : path.resolve(process.cwd(), options.destination);
         }
         if (!fs.existsSync(options.destination)) {
             fs.mkdirSync(options.destination, { recursive: true });
@@ -77,12 +57,9 @@ if (options.giveaway == 'webdav') {
     }
 }
 
-const HOME_DIR =
-    process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
+const HOME_DIR = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
 const monitorIdFile = path.join(HOME_DIR, '.exhaustive-twitter');
-const monitorId = fs.existsSync(monitorIdFile)
-    ? fs.readFileSync(monitorIdFile, { encoding: 'utf8' })
-    : uniqid('PFT');
+const monitorId = fs.existsSync(monitorIdFile) ? fs.readFileSync(monitorIdFile, { encoding: 'utf8' }) : uniqid('PFT');
 if (!fs.existsSync(monitorIdFile)) {
     fs.writeFileSync(monitorIdFile, monitorId, { flag: 'w+' });
 }
@@ -91,6 +68,14 @@ const term = options.term.split('-').map((term) => {
     const dt = term.split('T');
     return dt[0] + ' ' + dt[1];
 });
+if (options.keywords.length == 1) {
+    try {
+        console.log('JSON Keyword Mode');
+        options.keywords = JSON.parse(options.keywords[0]);
+    } catch (e) {
+        //Nothing To Do
+    }
+}
 const query = {
     title: options.id,
     from: DateTime.fromFormat(term[0], 'yyyy/MM/dd HH:mm').toISO(),
@@ -102,24 +87,25 @@ const query = {
     //urlsMatch:"text-or", //,"text-and" or "RegExp"
     //urls:[""],
     //lang: 'ja',
-    filters: [], //ignore_retweet, only_retweet
+    filters: {}, //ignore_retweet, only_retweet
     verbose: options.verbose ? true : false,
     jst: options.jst ? true : false,
 };
 if (options.mask) {
     query.mask = options.mask;
 } else {
-    query.mask =
-        'id_str,text,user(id_str,name,screen_name),is_quote_status,quoted_status_id_str,retweeted_status(id_str,user(id_str,name,screen_name)),entities(hashtags,user_mentions,urls),lang,timestamp_ms,created_at';
+    query.mask = 'id_str,text,user(id_str,name,screen_name),is_quote_status,quoted_status_id_str,retweeted_status(id_str,user(id_str,name,screen_name)),entities(hashtags,user_mentions,urls),lang,timestamp_ms,created_at';
 }
 if (options.lang) {
     query.lang = options.lang;
 }
 if (options.ignoreRetweet) {
-    query.filters.push('ignore_retweet');
+    //query.filters.push('ignore_retweet');
+    query.filters['ignore_retweet'] = true;
 }
 if (options.onlyRetweet) {
-    query.filters.push('only_retweet');
+    // query.filters.push('only_retweet');
+    query.filters['only_retweet'] = true;
 }
 if (options.giveaway == 'local') {
     query.giveaway = 'local';
@@ -127,6 +113,7 @@ if (options.giveaway == 'local') {
 } else if (options.giveaway == 'webdav') {
     query.giveaway = 'webdav';
     query.destination = options.destination;
+    query.user = options.user;
     query.password = password;
 } else {
     query.giveaway = 'no';
@@ -220,11 +207,7 @@ socket.on('connect', async () => {
         percent: true,
         y: 3,
     });
-    terminal
-        .moveTo(0, 4)
-        .defaultColor('Compress: ')
-        .yellow('...')
-        .eraseLineAfter();
+    terminal.moveTo(0, 4).defaultColor('Compress: ').yellow('...').eraseLineAfter();
     const uploadBar = terminal.progressBar({
         width: 70,
         title: 'Upload  :',
@@ -232,25 +215,17 @@ socket.on('connect', async () => {
         percent: true,
         y: 5,
     });
-    let job = new CronJob('* * * * * *', () => {
+    let job = new CronJob('*/2 * * * * *', () => {
         socket.emit('progress', { queryId, query }, (response) => {
+            //jobOnGoing = false;
             if (response.all != 0) {
-                if (
-                    response.done == response.all &&
-                    response.transfer.done == response.transfer.all &&
-                    response.glue.done == response.glue.all
-                ) {
+                if (response.done == response.all && response.transfer.done == response.transfer.all && response.glue.done == response.glue.all) {
                     job.stop();
                 }
                 progressBar.update(response.done / response.all);
-                transferBar.update(
-                    response.transfer.done / response.transfer.all
-                );
+                transferBar.update(response.transfer.done / response.transfer.all);
                 glueBar.update(response.glue.done / response.glue.all);
-                terminal
-                    .moveTo(11, 4)
-                    .yellow(response.compress)
-                    .eraseLineAfter();
+                terminal.moveTo(11, 4).yellow(response.compress).eraseLineAfter();
                 uploadBar.update(response.upload.done / response.upload.all);
                 //terminal.clear();
                 //terminal("Scan Files(done/all): "+response.done + '/' + response.all);
@@ -265,8 +240,7 @@ terminal.on('key', async function (name, matches, data) {
         if (!doubleCTRL_C) {
             doubleCTRL_C = true;
             terminal('\nQuit? [Y|n]\n');
-            let yn = await terminal.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] })
-                .promise;
+            let yn = await terminal.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] }).promise;
             if (yn) {
                 terminal.clear();
                 terminal.processExit();
